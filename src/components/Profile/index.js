f29400507df3d5b2fa83e52a4ed53e1756be55cb
lucas <luucasfarias21@gmail.com>
2020-01-16 22:16:42 -0300

    Create a new component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h2>Lucas Farias</h2>
+    <h3>Desenvolvedor Backend</h3>
+    <p>Um carinha legal</p>
+  </div>
+)
+
+export default Profile
